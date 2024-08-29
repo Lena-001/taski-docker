601d10a501605a1dbba1353b1dd209f0ec70acab
Елена Губаева <Kachechek1@yandex.ru>
2024-08-29 16:23:31 +0500

    v4

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -7,7 +7,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['158.160.6.179', '127.0.0.1', 'localhost', 'taskiii1.ddns.net']
+ALLOWED_HOSTS = ['158.160.6.179', '127.0.0.1', 'localhost', 'taskiii.ddns.net']
 
 
 # Application definition
